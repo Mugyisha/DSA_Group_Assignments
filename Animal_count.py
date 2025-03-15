38866080665d176e336cae0a497e4d81cc210766
Sadrack Echabu <111175137+sadrackechabu@users.noreply.github.com>
2025-03-15 12:06:38 +0300

    Add files via upload
    Animal Count

---
+++ Animal_count.py
@@ -0,0 +1,14 @@
+
+wild_animals = input("Please enter a group of wild animals you now: ")
+
+N = wild_animals.split()
+print(N)
+
+domestic_animals = input("Please enter a group of domestic animals you now: ")
+
+X= domestic_animals.split()
+print(X)
+
+animal_group = N + X
+
+print(animal_group)
