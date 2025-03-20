0d9ac720cc41c582a5aca60817a042a3005e9eea
Sadrack Echabu <111175137+sadrackechabu@users.noreply.github.com>
2025-03-20 13:46:36 +0300

    Animal_count_refactored.py
    Few changes made to source code Animal count

--- Animal_count.py
+++ Animal_count.py
@@ -1,14 +1,15 @@
+animals =[]
+wild_animals = input("Enter a group of wild animals of your choice: ").lower()
+# lion, monkey, baboon
+wild_animal_list = wild_animals.split(",")
+print(f"Your chosen list of Wild Animals: ", wild_animal_list)
+# cow, goat,cat
+home_animals = input("Enter a group of wild animals of your choice: ").lower()
+dom_animals = home_animals.split(",")
+print(f"Your chosen list of Domestic Animals: ", dom_animals)
 
-wild_animals = input("Please enter a group of wild animals you now: ")
-
-N = wild_animals.split()
-print(N)
-
-domestic_animals = input("Please enter a group of domestic animals you now: ")
-
-X= domestic_animals.split()
-print(X)
-
-animal_group = N + X
-
-print(animal_group)
+for animal in wild_animal_list:
+    animals.append(animal)
+for item in dom_animals:
+    animals.append(item)
+print(f"Here is your final list of animals: ", animals)
